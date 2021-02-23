ac7127364a5a6760a5ff81491fa6ce3eb42423fb
Magdalena Witerek <magdalenawiterek@gmail.com>
2021-02-23 20:32:09 +0100

    Revert "Zadania z modułu 2"
    This reverts commit 74d3a112

--- kodilla-intro/src/main/java/DwaSiedemIDwaOsiem/ZadanieDwaOsiem/ColorsApplication.java
+++
@@ -1,9 +0,0 @@
-package DwaSiedemIDwaOsiem.ZadanieDwaOsiem;
-
-import org.jetbrains.annotations.NotNull;
-
-public class ColorsApplication {
-    public static void main(String[] args) {
-        @NotNull String colors= Colors.getUserColor();
-    }
-}
